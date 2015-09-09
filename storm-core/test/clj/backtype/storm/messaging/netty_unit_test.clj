8beb4c23ec10cdf520de0130f4d35c9062fd990b
zhuol <zhuol@yahoo-inc.com>
2015-09-09 13:24:18 -0500

    minor

--- storm-core/test/clj/backtype/storm/messaging/netty_unit_test.clj
+++ storm-core/test/clj/backtype/storm/messaging/netty_unit_test.clj
@@ -28,7 +28,6 @@
 ;; clients directly, and thus we must ensure manually that the server and the client connections are ready before we
 ;; commence testing.  If we don't do this, then we will lose the first messages being sent between the client and the
 ;; server, which will fail the tests.
-
 (defn- wait-until-ready
   ([connections]
       (do (log-message "Waiting until all Netty connections are ready...")
